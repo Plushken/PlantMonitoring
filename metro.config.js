638c52049d4f85593a1ecf6f0b907a48a0151bc2
Plushken <96660351+Plushken@users.noreply.github.com>
2025-07-16 23:08:28 +0300

    refactor project settings

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-  const { transformer, resolver } = config;
+// Add SVG support to asset extensions for Expo Image
+config.resolver.assetExts.push('svg');
 
-  config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  };
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...resolver.sourceExts, 'svg'],
-  };
-
-  return config;
-})(); 
+module.exports = config; 
